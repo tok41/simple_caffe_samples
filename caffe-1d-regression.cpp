54cc0522c2e455e4ea09ce0347f6ac603e72ce67
tok-xc <tokita@xcompass.com>
2017-10-30 20:30:25 +0900

    add 1-dimention regression and util ipynb caffeで1次元の線形回帰を解く最もシンプルなモデルを作成。 データ作成と結果を確認するためのipynbも追加。

--- caffe-1d-regression.cpp
+++ caffe-1d-regression.cpp
@@ -1,53 +1,73 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 #include <memory>
 #include <random>
 #include <caffe/caffe.hpp>
 #include <caffe/layers/memory_data_layer.hpp>
 #include <glog/logging.h>
 
+
+void split(const std::string &s, char delim, std::function< void(int, std::string& )> f) {
+  std::string token;
+  std::istringstream stream(s);
+  int i=0;
+  while(getline(stream, token, ',')){
+    f(i, token);
+    //std::cout << i << ", " << token << std::endl;
+    i++;
+  }
+}
+
+
 int main(int argc, char** argv) {
   // glogの初期化
   google::InitGoogleLogging(argv[0]);
 
+  // データをロード
+  //std::ifstream ifs("data/sampledata_1d_linear_short.csv");
+  std::ifstream ifs("data/sampledata_discrete_target.csv");
+  if (ifs.fail())
+    {
+      std::cerr << "failed : file open" << std::endl;
+      return -1;
+    }
+  std::string line;
+  // ヘッダ部分のロード
+  std::vector<std::string> headerList;
+  getline(ifs, line);
+  split(line, ',', [&](int i, std::string & header) {
+      headerList.push_back(header);
+    });
+  // for (auto h: headerList)
+  //   std::cout << h << std::endl;
+
+  // データ部分のロード
+  std::map<int, std::vector<float>> dataMap;
+  while (getline(ifs, line))
+    {
+      split(line, ',', [&](int i, std::string & token) {
+	  dataMap[i].push_back(std::stof(token));
+	});
+    }
+  // for(auto itr=dataMap.begin(); itr != dataMap.end(); ++itr) {
+  //   std::cout << "key=" << itr->first << std::endl;
+  //   for(auto d: dataMap[itr->first])
+  //     std::cout << d << std::endl;
+  // }
+  std::cout << "data_size : " << dataMap[0].size() << std::endl;
+
+
   // パラメータ指定
   const int batch_size = 10;
-  const int data_size = 1000;
 
-  
 
-  constexpr auto kMinibatchSize = 32;
-  constexpr auto kDataSize = kMinibatchSize * 10;
-  
-  // 教師データとして用いる入力データと目標データをfloat配列として準備する．
-  // 入力データ：2次元
-  // 目標データ：1次元
-  std::array<float, kDataSize * 2> input_data;
-  std::array<float, kDataSize> target_data;
-  std::mt19937 random_engine; // MT法に夜乱数生成器
-  std::uniform_real_distribution<> dist(0.0, 1.0); // 一様分布のクラス
-
-  // 3*x_1 - 2*x_2 + 4 = target に従ってデータを生成する．
-  for (auto i = 0; i < kDataSize; ++i) {
-    const auto x_1 = dist(random_engine); //一様分布に従った乱数を生成
-    const auto x_2 = dist(random_engine);
-    const auto target = 3 * x_1 - 2 * x_2 + 4;
-    input_data[i * 2] = x_1;
-    input_data[i * 2 + 1] = x_2;
-    target_data[i] = target;
-  }
-
-  // データをファイルに出力
-  std::ofstream ofs("sample_data.csv");
-  for (auto i = 0; i < kDataSize; ++i) {
-    ofs<<input_data[i * 2]<<","<<input_data[i*2+1]<<","<<target_data[i]<<std::endl;
-  }
-
-  // MemoryDataLayerはメモリ上の値を出力できるDataLayer．
-  // 各MemoryDataLayerには入力データとラベルデータ（1次元の整数）の2つを与える必要があるが，
-  // ここでは回帰を行いたいので，入力データと目標データそれぞれを別のMemoryDataLayerで出力し，
-  // ラベルデータの代わりに使用されないダミーの値を与えておく．
-  std::array<float, kDataSize> dummy_data; // ダミーのラベル
-  std::fill(dummy_data.begin(), dummy_data.end(), 0.0);
+  // 読み込んだデータをcaffeのMemoryDataLayerに入力したい
+  // この問題は回帰を解きたいので、ラベルデータが存在しない
+  // だから、ダミーの値を用意しておく
+  std::vector<float> dummy_data( dataMap[0].size(), 0.0 );
 
   // Solverの設定をテキストファイルから読み込む
   caffe::SolverParameter solver_param;
@@ -59,32 +79,38 @@ int main(int argc, char** argv) {
   const auto input_layer =
     boost::dynamic_pointer_cast<caffe::MemoryDataLayer<float>>(net->layer_by_name("input"));
   assert(input_layer);
-  input_layer->Reset(input_data.data(), dummy_data.data(), kDataSize);
-
+  input_layer->Reset(dataMap[0].data(), dummy_data.data(), dataMap[0].size());
+  
   // 目標データをMemoryDataLayer"target"にセットする
   const auto target_layer =
     boost::dynamic_pointer_cast<caffe::MemoryDataLayer<float>>(net->layer_by_name("target"));
   assert(target_layer);
-  target_layer->Reset(target_data.data(), dummy_data.data(), kDataSize);
-
+  target_layer->Reset(dataMap[1].data(), dummy_data.data(), dataMap[1].size());
+  
   // Solverの設定通りに学習を行う
   solver->Solve();
+  
 
-  // 学習されたパラメータを出力してみる
-  // ax + by + c = target
-  const auto ip_blobs = net->layer_by_name("ip")->blobs();
-  const auto learned_a = ip_blobs[0]->cpu_data()[0];
-  const auto learned_b = ip_blobs[0]->cpu_data()[1];
-  const auto learned_c = ip_blobs[1]->cpu_data()[0];
-  std::cout << learned_a << "x_1 + " << learned_b << "x_2 + " << learned_c
-	    << " = target" << std::endl;
+  // 学習されたモデルを使って予測
+  // 結果をファイルに出力
+  std::ofstream ofs("data/predicted_result.csv");
+  // x ~ [0, 1.0]
+  std::random_device rand_dev; // 乱数のシードを決めるために
+  std::mt19937 mt(rand_dev());
+  std::uniform_real_distribution<> dist(0.0, 9.0); // 一様分布のクラス
 
-  // 学習されたモデルを使って予測してみる
-  // x = 10, y = 20
-  std::array<float, kDataSize * 2> sample_input;
-  sample_input[0] = 10;
-  sample_input[1] = 20;
-  input_layer->Reset(sample_input.data(), dummy_data.data(), kDataSize);
-  net->ForwardPrefilled(nullptr);
-  std::cout << "10a + 20b + c = " << net->blob_by_name("ip")->cpu_data()[0] << std::endl;
+  int n_test = 100; // テストの回数
+  for(int n=0 ; n<(int)(n_test/batch_size) ; n++){
+    std::vector<float> sample_input;
+    for(int i=0 ; i<batch_size ; i++) { // バッチ分のデータを入力
+      sample_input.push_back(dist(mt));
+    }
+    input_layer->Reset(sample_input.data(), dummy_data.data(), sample_input.size());
+    const auto result = net->Forward();
+    for(int i=0 ; i<sample_input.size() ; i++){
+      //std::cout << "x=" << sample_input[i] << ", y=" << net->blob_by_name("ip")->cpu_data()[i] << std::endl;
+      ofs << sample_input[i] << "," << net->blob_by_name("ip")->cpu_data()[i] << std::endl;
+    }
+  }
+  
 }
